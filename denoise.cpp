21b7d7d9362acbcb99365467b0659a464e05637f
lustigepepe <lustigepepe@web.de>
2016-03-15 23:52:59 +0100

    Final

--- denoise.cpp
+++ denoise.cpp
@@ -24,25 +24,26 @@
 
 // declaration the methode the important methode, using by VLC
 static int Open (vlc_object_t *);
+static void Close (vlc_object_t *);
 static block_t* Process (filter_t *, block_t *);
 
 // Module descriptor
 vlc_module_begin ()
-    set_shortname (N_("Denoise"))
-    set_description (N_("Denoise that Song"))
-    // kind of audio / #define  CAT_AUDIO   2
-    set_category (CAT_AUDIO)
-    set_subcategory (SUBCAT_AUDIO_AFILTER)
-    // kind of module
-    set_capability ("audio filter", 0)
-    set_callbacks (Open, NULL)
+set_shortname (N_("Denoise"))
+set_description (N_("Denoise that Song"))
+// kind of audio / #define  CAT_AUDIO   2
+set_category (CAT_AUDIO)
+set_subcategory (SUBCAT_AUDIO_AFILTER)
+// kind of module
+set_capability ("audio filter", 0)
+set_callbacks (Open, Close)
 vlc_module_end ()
 
 
-// Starts that module 
-static int Open (vlc_object_t *obj)
+// Start that module
+static int Open (vlc_object_t* obj)
 {
-    filter_t *filter = (filter_t *)obj;
+    filter_t* filter = (filter_t *)obj;
 
     if (filter->fmt_in.audio.i_channels != 2)
     {
@@ -50,9 +51,9 @@ static int Open (vlc_object_t *obj)
         return VLC_EGENERIC;
     }
 
-    // Allocate structure 
+    // Allocate structure && definate frameBuffer
     filter_sys_t* p_sys = filter->p_sys = new filter_sys_t;
-    if( !p_sys )
+    if (!p_sys)
         return VLC_ENOMEM;
 
     filter->fmt_in.audio.i_format = VLC_CODEC_FL32;
@@ -61,61 +62,67 @@ static int Open (vlc_object_t *obj)
     return VLC_SUCCESS;
 }
 
+static void Close (vlc_object_t* obj) {
+
+    filter_t* filter = (filter_t *)obj;
+    // free the your allocated memory
+    delete[] filter->p_sys;
+
+}
+
 
 // Main methode with the work
-static block_t* Process (filter_t *filter, block_t *block)
-{
+static block_t* Process (filter_t* filter, block_t* block) {
     // i_nb_samples get the size of sample
     const unsigned sampleSize = block->i_nb_samples;
     // create a size with a potenz of 2
-    int N = (int) createN(block->i_nb_samples);
+    int n = (int) createN(block->i_nb_samples);
     // the p_buffer have all audio data
-    float *spl = (float *)block->p_buffer;
-    float *temp_buffer_ = new float[N];
+    float* spl = (float *)block->p_buffer;
+    float* temp_buffer_ = new float[n];
 
 
     // real and imagin part/ for the fft
-    frequenzStruct *fftStruct = new frequenzStruct;
+    frequenzStruct* fftStruct = new frequenzStruct;
 
-    fftStruct->xr = new float[N];
-    fftStruct->xi = new float[N];
+    fftStruct->xr = new float[n];
+    fftStruct->xi = new float[n];
 
     // to get only one chanel
     unsigned mono = sampleSize * 2;
 
-    // fill the buffer with all data 
-    float* temp_buffer = fillBuffer(N, sampleSize, mono, temp_buffer_, spl);
+    // fill the buffer with all data
+    float* temp_buffer = fillBuffer(n, sampleSize, mono, temp_buffer_, spl);
 
 
     // build denoice mask
     // start with fft analyse
     // fill noise struct
-    frequenzStruct* fft_Struct = fillStruct(N, temp_buffer, fftStruct);
+    frequenzStruct* fft_Struct = fillStruct(n, temp_buffer, fftStruct);
     // sort the sampling points for the fft
-    frequenzStruct* fftStr = inversion(N, fft_Struct);
+    frequenzStruct* fftStr = inversion(n, fft_Struct);
     // transform samples to the frequenz Area
-    frequenzStruct* forNoiseMask = allInOnefft(N, fftStr, 0);
-    // create the noise - mask   
-    frequenzStruct* frequenzArea = fillNoiseMask(N, filter, forNoiseMask, 39);
+    frequenzStruct* forNoiseMask = allInOnefft(n, fftStr, 0);
+    // create the noise - mask
+    frequenzStruct* frequenzArea = fillNoiseMask(n, sampleSize, filter, forNoiseMask, 39);
 
     // if noise mask created -> start with denoise
-    // if(filter->p_sys->maskComplete){
-
-    frequenzStruct* frequenzAreaCo = denoise(N, filter, frequenzArea);
-    // }
+    frequenzStruct* frequenzAreaCo = denoise(n, filter, frequenzArea);
 
 
     // prepare fft
-    frequenzStruct* fftStrTwo = inversion(N, frequenzAreaCo);
+    frequenzStruct* fftStrTwo = inversion(n, frequenzAreaCo);
     // back to time - area
-    frequenzStruct* timeArea = allInOnefft(N, fftStrTwo, 1);
+    frequenzStruct* timeArea = allInOnefft(n, fftStrTwo, 1);
     // reduce the output fo fft
-    frequenzStruct* timeAreaI = overdriveCutOut(N, timeArea);
-    // get denoised data
-    float* temp_bufferFinal = timeAreaI->xr;
-    // set data back to the block 
-    for (unsigned i = sampleSize; i > 0; i--){
-        
+    frequenzStruct* timeAreaI = overdriveCutOut(n, timeArea);
+
+    // window overlapping function
+    float* temp_bufferFinal = windowOverlapping(sampleSize, filter, timeAreaI);
+
+    // set data back to the block
+    for (unsigned i = sampleSize; i > 0; i--) {
+
         // channel = audio track for mono stereo
         float s = *(temp_bufferFinal++);
         // channel stereo
@@ -124,15 +131,6 @@ static block_t* Process (filter_t *filter, block_t *block)
     }
 
     (void) filter;
-    // finish get block back
+    // finish get the block back
     return block;
 }
-
-
-
-
-
-
-
-
-
--- denoise.hpp
+++ denoise.hpp
@@ -2,52 +2,54 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
- 
 
 /* VLC core API headers */
 #include <vlc_common.h>
 #include <vlc_aout.h>
 #include <vlc_filter.h>
 #include <vlc_plugin.h>
-#include <vector>
 
- /* need it for my filter */
+/* need it explicitly for the denoise filter */
 #include <assert.h>
 #include <math.h>
 #include <stdlib.h>
+#include <vector>
 
-// noiseMask struct
-struct filter_sys_t
-{
-    //Filter static config 
+// noiseMask + windowBuffer struct
+struct filter_sys_t {
+    //Filter static config
     std::vector<float> noiseDensity;
-    // give the grad of complete 
+    // used for window overlapping
+    float* window;
+    float* secondWindow;
+    // marker for the level of using window
+    int windowZero = 0;
+    // marker for the start of overlapping
+    int overlappingStart = 0;
+    // give the grad of complete
     int positions = 1;
     bool maskComplete = false;
+    // noiseMAsk
     float* noise_xr;
     float* noise_xi;
 
-   
 };
-
-
 
 // using by fft real and imaginary part
-struct frequenzStruct{
+struct frequenzStruct {
     float* xr;
     float* xi;
-    
+
 };
 
-
 // get the size of the potens of 2
-unsigned createN(unsigned samples){
+unsigned createN(unsigned samples) {
 
 
     double param, fractpart, intpart;
-    param = log(samples)/ log(2);
+    param = log(samples) / log(2);
     fractpart = modf (param , &intpart);
-    if (fractpart > 0){
+    if (fractpart > 0) {
         intpart++;
     }
 
@@ -56,14 +58,14 @@ unsigned createN(unsigned samples){
 }
 
 
-float* fillBuffer(unsigned N, unsigned sampleSize, unsigned mono, float* temp_buffer, float* spl){
-    for (unsigned i = N; i > 0; i--){
+float* fillBuffer(unsigned N, unsigned sampleSize, unsigned mono, float* temp_buffer, float* spl) {
+    for (unsigned i = N; i > 0; i--) {
         // zero Pattern / set all not used point to 0
-        if (i > sampleSize){
-            temp_buffer[i-1] = 0.0f;
+        if (i > sampleSize) {
+            temp_buffer[i - 1] = 0.0f;
         } else {
-            mono--; 
-            temp_buffer[i-1] = *(spl+mono--);
+            mono--;
+            temp_buffer[i - 1] = *(spl + mono--);
 
         }
 
@@ -72,24 +74,22 @@ float* fillBuffer(unsigned N, unsigned sampleSize, unsigned mono, float* temp_bu
     return temp_buffer;
 }
 
-frequenzStruct* fillStruct(int n, float* temp_buffer, frequenzStruct* fS){
+frequenzStruct* fillStruct(int n, float* temp_buffer, frequenzStruct* fS) {
     // initial fill / imaginary part part is allways 0
-    for (int i=0;i<n;i++)
-    {
+    for (int i = 0; i < n; i++) {
         fS->xr[i] = temp_buffer[i];
         fS->xi[i] = 0.0f;
-    }  
+    }
 
     return fS;
 
 }
 
-
-frequenzStruct* inversion(int n, frequenzStruct* fS){
+frequenzStruct* inversion(int n, frequenzStruct* fS) {
     int i, j, k;
     float hr, hi;
-    for (i = j = 1, k = n/2; i < n; i++, j += k, k = n / 2){
-        if (j > i){
+    for (i = j = 1, k = n / 2; i < n; i++, j += k, k = n / 2) {
+        if (j > i) {
             hr = fS->xr[i - 1];
             hi = fS->xi[i - 1];
             fS->xr[i - 1] = fS->xr[j - 1];
@@ -102,55 +102,54 @@ frequenzStruct* inversion(int n, frequenzStruct* fS){
 
     return fS;
 
-
 }
 
-frequenzStruct* overdriveCutOut(int n, frequenzStruct* fS){
+frequenzStruct* overdriveCutOut(int n, frequenzStruct* fS) {
     float factor = 1;
     // important part reduce with the length of samples
     factor /= n;
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        fS->xr[i] *=factor;
-    }  
+        fS->xr[i] *= factor;
+    }
     return fS;
 }
 
 
-frequenzStruct* allInOnefft(int n, frequenzStruct* fS, int typ){
+frequenzStruct* allInOnefft(int n, frequenzStruct* fS, int typ) {
 
-    int i,j,k,l,m;
+    int i, j, k, l, m;
     int le;
-    float hr,hi;
+    float hr, hi;
     float wr, wi, whr, whi;
-    for (i = n, m = 0; i>1; m++, i/=2);
-    for (i = 0; i < m; i++ ){
+    for (i = n, m = 0; i > 1; m++, i /= 2);
+    for (i = 0; i < m; i++ ) {
         le = 1 << (i + 1);
         whr = 1;
         whi = 0;
         // to the frequenz-area
-        if (typ == 0){
-            wr = (float)cos(M_PI/ le * 2);
-            wi = (float)-sin(M_PI/ le * 2);
+        if (typ == 0) {
+            wr = (float)cos(M_PI / le * 2);
+            wi = (float) - sin(M_PI / le * 2);
         } else {
-        // to the time-area 
-            wr = (float)cos(M_PI/ le * 2);
-            wi = (float)sin(M_PI/ le * 2);
+            // to the time-area
+            wr = (float)cos(M_PI / le * 2);
+            wi = (float)sin(M_PI / le * 2);
         }
-        for (j = 0; j < le / 2; j++){
-            for(k = j; k < n; k += le){
+        for (j = 0; j < le / 2; j++) {
+            for (k = j; k < n; k += le) {
                 l = k + le / 2;
                 hr = fS->xr[l] * whr - fS->xi[l] * whi;
                 hi = fS->xi[l] * whr + fS->xr[l] * whi;
                 fS->xr[l] = fS->xr[k] - hr;
                 fS->xi[l] = fS->xi[k] - hi;
                 fS->xr[k] = fS->xr[k] + hr;
-                fS->xi[k] = fS->xi[k] + hi; 
+                fS->xi[k] = fS->xi[k] + hi;
             }
             hr = whr * wr - whi * wi;
-            hi = whi * wr + whr * wi; 
+            hi = whi * wr + whr * wi;
             whr = hr;
-            whi = hi; 
+            whi = hi;
 
         }
 
@@ -161,47 +160,47 @@ frequenzStruct* allInOnefft(int n, frequenzStruct* fS, int typ){
 }
 
 // set the average of noise data
-bool averageAmplitude(int n, filter_t* filter){
-    
+bool averageAmplitude(int n, filter_t* filter) {
+
     int length = filter->p_sys->positions;
     float xr = 0;
     float xi = 0;
     float density = 0;
 
-    // add all date divide with count  
-    for (int i = 0; i < n; i++){
-    
-        for (int j = 1; j <= length; j++){
-        
-            density += filter->p_sys->noiseDensity[i*j];
-            xr += filter->p_sys->noise_xr[i*j];
-            xi += filter->p_sys->noise_xi[i*j];
+    // add all date divide with count
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 1; j <= length; j++) {
+
+            density += filter->p_sys->noiseDensity[i * j];
+            xr += filter->p_sys->noise_xr[i * j];
+            xi += filter->p_sys->noise_xi[i * j];
         }
 
         filter->p_sys->noiseDensity[i] = density /= length;
         filter->p_sys->noise_xr[i] = xr /= length;
-        filter->p_sys->noise_xi[i] = xi /= length;    
+        filter->p_sys->noise_xi[i] = xi /= length;
     }
     filter->p_sys->noiseDensity.resize(n);
     return filter->p_sys->maskComplete = true;
-   
+
 }
 
 
 // charge the power of density for all re and im
-std::vector<float> powerDensity(int N, frequenzStruct* str, int frames){
-    
-    // vector for size of frames 
-    std::vector<float> temp(N*frames);
-    for (int i = 0; i < N; i++){
+std::vector<float> powerDensity(int n, frequenzStruct* str, int frames) {
+
+    // vector for size of frames
+    std::vector<float> temp(n * frames);
+    for (int i = 0; i < n; i++) {
         temp[i] = sqrt(str->xr[i] * str->xr[i] + str->xi[i] * str->xi[i]);
     }
     return temp;
 }
 
-frequenzStruct* fillNoiseMask(unsigned n, filter_t* filter, frequenzStruct* data, int frames){
+frequenzStruct* fillNoiseMask(unsigned n, int s, filter_t* filter, frequenzStruct* data, int frames) {
     // if finished get out
-    if (filter->p_sys->maskComplete){
+    if (filter->p_sys->maskComplete) {
 
         return data;
     }
@@ -211,47 +210,47 @@ frequenzStruct* fillNoiseMask(unsigned n, filter_t* filter, frequenzStruct* data
     int position = filter->p_sys->positions;
     std::vector<float> temp = powerDensity(n, data, 1);
 
-    // if noise mask allready existing but not finished fill with more data 
-    if (filter->p_sys->noiseDensity.size() != 0){
+    // if noise mask allready existing but not finished fill with more data
+    if (filter->p_sys->noiseDensity.size() != 0) {
         int value = 0;
-        for (unsigned k = filter->p_sys->noiseDensity.size() ; k < n*position; k++){
+        for (unsigned k = filter->p_sys->noiseDensity.size() ; k < n * position; k++) {
 
             filter->p_sys->noiseDensity[k] = temp[value];
             filter->p_sys->noise_xr[k] = data->xr[value];
             filter->p_sys->noise_xi[k] = data->xi[value++];
         }
-        filter->p_sys->noiseDensity.resize(n*position); 
-        // if it's complete build average of data 
-        if (filter->p_sys->positions == frames){
+        filter->p_sys->noiseDensity.resize(n * position);
+        // if it's complete build average of data
+        if (filter->p_sys->positions == frames) {
             bool average = averageAmplitude( n, filter);
 
-            return data;   
+            return data;
         }
         filter->p_sys->positions++;
-        
-        return data;   
+
+        return data;
     }
 
-    for (unsigned i = 0; i < n; i++){
+    for (unsigned i = 0; i < n; i++) {
         // only for the first data initial noise struct
-        if (filter->p_sys->noiseDensity.size() == 0 && temp[i] != 0.0f){
+        if (filter->p_sys->noiseDensity.size() == 0 && temp[i] != 0.0f) {
             flag = 1;
             firstMatch = i;
-            filter->p_sys->noise_xr = new float[n*frames];
-            filter->p_sys->noise_xi = new float[n*frames];
+            filter->p_sys->noise_xr = new float[n * frames];
+            filter->p_sys->noise_xi = new float[n * frames];
             break;
         }
     }
-    // set first data for the mask 
-    if (filter->p_sys->noiseDensity.size() == 0 && flag == 1){
-        std::vector<float> init(n*frames);
+    // set first data for the mask
+    if (filter->p_sys->noiseDensity.size() == 0 && flag == 1) {
+        std::vector<float> init(n * frames);
         filter->p_sys->noiseDensity = init;
         // if using data not start at the first position skip
-        if (firstMatch != 0){
-            filter->p_sys->noiseDensity.resize(n*frames-1);
+        if (firstMatch != 0) {
+            filter->p_sys->noiseDensity.resize(n * frames - 1);
             return data;
         }
-        for(unsigned j = 0; j < n; j++){
+        for (unsigned j = 0; j < n; j++) {
             filter->p_sys->noiseDensity[j] = temp[j];
             filter->p_sys->noise_xr[j] = data->xr[j];
             filter->p_sys->noise_xi[j] = data->xi[j];
@@ -267,18 +266,18 @@ frequenzStruct* fillNoiseMask(unsigned n, filter_t* filter, frequenzStruct* data
 
 }
 
-
-frequenzStruct* denoise(int n, filter_t* filter, frequenzStruct* song){
+// denoise that signal
+frequenzStruct* denoise(int n, filter_t* filter, frequenzStruct* song) {
 
     if (!filter->p_sys->maskComplete)
         return song;
-    std::vector<float> songBuffer = powerDensity(n,song,1);
+    std::vector<float> songBuffer = powerDensity(n, song, 1);
     std::vector<float> noiseBuffer = filter->p_sys->noiseDensity;
-    
+
     // all data they are smaller or equal that gate -> reduce
-    for (int i = 0; i < n; i++){
-        
-        if (songBuffer[i] <= noiseBuffer[i]*2000){
+    for (int i = 0; i < n; i++) {
+
+        if (songBuffer[i] <= noiseBuffer[i] * 2000) {
             song->xr[i] = 0.0f;
             song->xi[i] = 0.0f;
         }
@@ -287,3 +286,157 @@ frequenzStruct* denoise(int n, filter_t* filter, frequenzStruct* song){
     return song;
 }
 
+// calculate value of fadein
+float fadeIn(float i, float length) {
+
+    i++;
+    i = i / length;
+    return i;
+}
+
+// calculate value of fadeout
+float fadeOut(float i, float length) {
+
+    i++;
+    i = 1 - i / length;
+    return i;
+}
+
+// get the size of the potens of 2
+int createFadingPieace(double samples) {
+
+
+    double param, fractpart, intpart;
+    param = samples / 2;
+
+    // reduce for a little fade
+    fractpart = modf (param , &intpart);
+    if (fractpart > 0) {
+        intpart--;
+    }
+
+    return param;
+
+}
+
+
+
+// prepare window one for the addition
+float* editWindowOne(int fadingWindow, int startFade, float* windowOne) {
+
+    for (int i = 0; i < fadingWindow; i++) {
+        windowOne[startFade++] *= fadeOut(i, fadingWindow);
+
+    }
+
+    return windowOne;
+}
+
+// prepare window two for the addition
+float* editWindowTwo(int fadingWindow, float* windowTwo) {
+
+    for (int i = 0; i < fadingWindow; i++) {
+        windowTwo[i] *= fadeIn(i, fadingWindow);
+
+    }
+
+    return windowTwo;
+}
+
+// create window three for the addition
+float* fillWindowThree(int fadingWindow, int startFade, float* windowOne, float* windowTwo) {
+
+    // Size of the window three
+    int windowThreeSize = fadingWindow * 2;
+
+    // initial value of window three
+    float* windowThree = new float[windowThreeSize];
+
+    for (int s = 0; s < fadingWindow; s++) {
+        windowThree[s] = windowOne[startFade++];
+        windowThree[fadingWindow + s] = windowTwo[s];
+    }
+
+
+    // all fading position in window three
+    for (int i = 0; i < fadingWindow; i++) {
+        windowThree[i] *= fadeIn(i, fadingWindow);
+
+    }
+
+    for (int j = fadingWindow; j < windowThreeSize; j++) {
+        windowThree[j] *= fadeOut(j, fadingWindow);
+
+    }
+
+    return windowThree;
+}
+
+// overlapped the frames for a better sound
+float* windowOverlapping(int n, filter_t* filter, frequenzStruct* song) {
+    if (!filter->p_sys->maskComplete)
+        return song->xr;
+
+    // fill initial the windowbuffer
+    if (filter->p_sys->overlappingStart == 0) {
+        filter->p_sys->overlappingStart = 1;
+
+        // initialize the windows
+        filter->p_sys->window = new float[n];
+        filter->p_sys->secondWindow = new float[n];
+
+        // set window marker to in use
+        filter->p_sys->windowZero = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            filter->p_sys->window[i] = song->xr[i];
+            song->xr[i] = 0.0f;
+        }
+
+        return song->xr;
+    }
+
+    // if there only a sconde window buffer
+    // then fill the first window also
+    if (filter->p_sys->windowZero == 0) {
+
+        // set window marker to in use
+        filter->p_sys->windowZero = 1;
+
+        for (int i = 0; i < n; i++) {
+            filter->p_sys->window[i] = song->xr[i];
+        }
+
+        return filter->p_sys->secondWindow;
+    }
+
+    // // only for fade_ in and out
+    int fadingWindow = createFadingPieace(n);
+
+    // // Startpoint of fade
+    int startFade = n - fadingWindow;
+
+    // // create the windows for overlapping
+    float* three = fillWindowThree(fadingWindow, startFade, filter->p_sys->window, song->xr);
+    float* one = editWindowOne(fadingWindow, startFade, filter->p_sys->window);
+    float* two = editWindowTwo(fadingWindow, song->xr);
+
+    // mix the windows
+    int justAdditionTwo = fadingWindow;
+    for (int x = 0; x < fadingWindow; x++) {
+        one[startFade++] += three[x];
+        two[x] += three[justAdditionTwo++];
+
+    }
+
+    // fill the second window buffer for the next frame
+    for (int i = 0; i < n; i++) {
+        filter->p_sys->secondWindow[i] = two[i];
+    }
+
+    // the first window buffer is no longer used, set it to 0
+    filter->p_sys->windowZero = 0;
+
+    return one;
+}
